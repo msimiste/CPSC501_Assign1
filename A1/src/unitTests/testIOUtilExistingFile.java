3ded5a94c7d00423cb651a5971ebad55df4964b3
msimiste <mikesimister@shaw.ca>
2016-10-12 20:06:16 -0600

    Refactoring_2: unit tests added unit tests for original utility methods passing

---
+++ A1/src/unitTests/testIOUtilExistingFile.java
@@ -0,0 +1,62 @@
+package unitTests;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import workingFiles.IOUtility;
+
+public class testIOUtilExistingFile {
+
+	
+	@Before public void setUp(){
+		String catalogFilePath = "catalog.txt";
+		File catalogFile = new File(catalogFilePath);
+		
+		try{
+			catalogFile.createNewFile();
+		}
+		catch(Exception e){}
+	}
+	
+	@After public void tearDown(){
+		String catalogFilePath = "catalog.txt";
+		File catalogFile = new File(catalogFilePath);
+		
+		try{
+			catalogFile.delete();
+		}
+		catch(Exception e){}
+	}	
+	
+	@Test
+	public void testCheckLocalCache_WhenLocalFileExists() {
+		
+		String catalogFilePath = "catalog.txt";
+		File catalogFile = new File(catalogFilePath);
+		
+		try{
+			catalogFile.createNewFile();
+		}
+		catch(Exception e){}
+		
+		IOUtility ioUtil = new IOUtility();
+		boolean cacheExists = true;
+		
+		cacheExists = ioUtil.checkLocalCache();
+		
+		assertEquals(cacheExists,true);
+		
+		try{
+			catalogFile.delete();
+		}
+		catch(Exception e){}
+		
+	}
+
+}
---
+++ A1/src/unitTests/testIOUtilNoFile.java
@@ -0,0 +1,54 @@
+package unitTests;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import workingFiles.IOUtility;
+
+public class testIOUtilNoFile {
+
+	@Before
+	public void setUP() {
+		String catalogFilePath = "catalog.txt";
+		File catalogFile = new File(catalogFilePath);
+
+		if (catalogFile.exists()) {
+			try {
+				catalogFile.delete();
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	@Test
+	public void testCheckLocalCache_WhenLocalFileExists() {
+
+		String catalogFilePath = "catalog.txt";
+		File catalogFile = new File(catalogFilePath);
+
+		try {
+			catalogFile.createNewFile();
+		} catch (Exception e) {
+		}
+
+		IOUtility ioUtil = new IOUtility();
+		boolean cacheExists = true;
+
+		cacheExists = ioUtil.checkLocalCache();
+
+		assertEquals(cacheExists, true);
+
+		try {
+			catalogFile.delete();
+		} catch (Exception e) {
+		}
+
+	}
+
+}
